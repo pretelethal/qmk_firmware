898f1a89ca8f7ea32d5932ff5eb82e2084de1027
pretelethal <pretelethal@stofflejp@gmail.com>
2021-02-08 17:13:15 -0500

    remove unused keyboard profiles and add test keymap to ctrl

---
+++ keyboards/massdrop/ctrl/keymaps/jay/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "config_common.h"
+
+#define FORCE_NKRO
+
+#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_COLOR
+#define RGB_MATRIX_STARTUP_VAL 100
+#define HSV_BACKLIGHT_COLOR HSV_WHITE
